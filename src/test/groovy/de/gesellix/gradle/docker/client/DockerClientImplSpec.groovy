d28815b74362acd973831d5a02b40d5b0cc57ec1
Tobias Gesellchen <tobias@gesellix.de>
2014-04-27 14:40:08 +0200

    move response from inlined code to external resource

--- src/test/groovy/de/gesellix/gradle/docker/client/DockerClientImplSpec.groovy
+++ src/test/groovy/de/gesellix/gradle/docker/client/DockerClientImplSpec.groovy
@@ -1,15 +1,14 @@
 package de.gesellix.gradle.docker.client
 
 import com.jayway.restassured.RestAssured
+import com.xebialabs.restito.semantics.Action
+import com.xebialabs.restito.semantics.Condition
 import com.xebialabs.restito.server.StubServer
 import spock.lang.Shared
 import spock.lang.Specification
 
 import static com.xebialabs.restito.builder.stub.StubHttp.whenHttp
 import static com.xebialabs.restito.builder.verify.VerifyHttp.verifyHttp
-import static com.xebialabs.restito.semantics.Action.status
-import static com.xebialabs.restito.semantics.Action.stringContent
-import static com.xebialabs.restito.semantics.Condition.*
 import static org.glassfish.grizzly.http.Method.POST
 import static org.glassfish.grizzly.http.util.HttpStatus.OK_200
 
@@ -41,21 +40,19 @@ class DockerClientImplSpec extends Specification {
   def "pull image"() {
     given:
     whenHttp(server).
-        match(post("/images/create"),
-              parameter("fromImage", "scratch")).
-        then(status(OK_200),
-             stringContent("{\"status\":\"Pulling repository scratch\"}\r\n{\"status\":\"Pulling image (latest) from scratch\",\"progressDetail\":{},\"id\":\"511136ea3c5a\"}{\"status\":\"Pulling image (latest) from scratch, endpoint: https://cdn-registry-1.docker.io/v1/\",\"\
-      progressDetail\":{},\"id\":\"511136ea3c5a\"}{\"status\":\"Pulling dependent layers\",\"progressDetail\":{},\"id\":\"511136ea3c5a\"}{\"status\":\"Download complete\",\"progressDetail\":{},\"id\":\"511136ea3c5a\"}{\"status\":\"Download complete\",\"progressDetail\"\
-      :{},\"id\":\"511136ea3c5a\"}"));
+        match(Condition.post("/images/create"),
+              Condition.parameter("fromImage", "scratch")).
+        then(Action.status(OK_200),
+             Action.resourceContent(getClass().getResource("pull_image_responses.chunked.json")));
 
     when:
     def imageId = dockerClient.pull("scratch")
 
     then:
     verifyHttp(server).once(
-        method(POST),
-        uri("/images/create"),
-        parameter("fromImage", "scratch")
+        Condition.method(POST),
+        Condition.uri("/images/create"),
+        Condition.parameter("fromImage", "scratch")
     )
 
     and:
