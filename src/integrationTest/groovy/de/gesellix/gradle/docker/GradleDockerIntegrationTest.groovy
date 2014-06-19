fb2a66b9b95effc7b320623688dd4f8caf99fe63
Tobias Gesellchen <tobias@gesellix.de>
2014-06-19 14:10:18 +0200

    testing gradle 2.0-rc1

--- src/integrationTest/groovy/de/gesellix/gradle/docker/GradleDockerIntegrationTest.groovy
+++ src/integrationTest/groovy/de/gesellix/gradle/docker/GradleDockerIntegrationTest.groovy
@@ -3,19 +3,29 @@ package de.gesellix.gradle.docker
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Ignore
+import spock.lang.Shared
 import spock.lang.Specification
 
 @Ignore("only for exploring the docker api")
 class GradleDockerIntegrationTest extends Specification {
 
-  def "test"() {
-    given:
-    Project project = ProjectBuilder.builder().build()
+  @Shared
+  Project project
+  @Shared
+  def projectDir
+
+  def setup() {
+    URL resource = getClass().getResource('/example.gradle')
+    projectDir = new File(resource.toURI()).getParentFile()
+    project = ProjectBuilder.builder().withName('example').withProjectDir(projectDir).build()
+  }
+
+  def "test deploy"() {
     when:
-    project.apply plugin: 'gradle-docker'
     def task = project.task('dockerDeploy', type: DockerDeployTask)
+    task.imageName = 'scratch'
 
     then:
-    task.deploy() == null
+    task.deploy() == '511136ea3c5a'
   }
 }
