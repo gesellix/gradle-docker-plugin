e8b7de65b78ddb87b4d917a9b3c6b1f31796adc9
Tobias Gesellchen <tobias.gesellchen@hypoport.de>
2014-07-24 10:13:27 +0200

    add rmi task
    closes #5

---
+++ src/main/groovy/de/gesellix/gradle/docker/tasks/DockerRmiTask.groovy
@@ -0,0 +1,20 @@
+package de.gesellix.gradle.docker.tasks
+
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class DockerRmiTask extends AbstractDockerTask {
+
+  private static Logger logger = LoggerFactory.getLogger(DockerRmiTask)
+
+  @Input
+  def imageId
+
+  @TaskAction
+  def rmi() {
+    logger.info "running rmi..."
+    getDockerClient().rmi(getImageId())
+  }
+}
---
+++ src/test/groovy/de/gesellix/gradle/docker/tasks/DockerRmiTaskSpec.groovy
@@ -0,0 +1,29 @@
+package de.gesellix.gradle.docker.tasks
+
+import de.gesellix.docker.client.DockerClient
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class DockerRmiTaskSpec extends Specification {
+
+  def project
+  def task
+  def dockerClient = Mock(DockerClient)
+
+  def setup() {
+    project = ProjectBuilder.builder().build()
+    task = project.task('dockerRmi', type: DockerRmiTask)
+    task.dockerClient = dockerClient
+  }
+
+  def "delegates to dockerClient"() {
+    given:
+    task.imageId = "4712"
+
+    when:
+    task.rmi()
+
+    then:
+    1 * dockerClient.rmi("4712")
+  }
+}
