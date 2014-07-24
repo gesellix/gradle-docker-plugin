2aae5b0e3968c29cc767fee723691800cfbb59a7
Tobias Gesellchen <tobias.gesellchen@hypoport.de>
2014-07-24 09:13:50 +0200

    add tag task
    closes #4

---
+++ src/test/groovy/de/gesellix/gradle/docker/tasks/DockerTagTaskSpec.groovy
@@ -0,0 +1,30 @@
+package de.gesellix.gradle.docker.tasks
+
+import de.gesellix.docker.client.DockerClient
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class DockerTagTaskSpec extends Specification {
+
+  def project
+  def task
+  def dockerClient = Mock(DockerClient)
+
+  def setup() {
+    project = ProjectBuilder.builder().build()
+    task = project.task('dockerTag', type: DockerTagTask)
+    task.dockerClient = dockerClient
+  }
+
+  def "delegates to dockerClient"() {
+    given:
+    task.imageId = "4711"
+    task.tag = "aTag"
+
+    when:
+    task.tag()
+
+    then:
+    1 * dockerClient.tag("4711", "aTag")
+  }
+}
