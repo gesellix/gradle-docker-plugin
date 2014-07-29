20e7b641bd71ccf9097c582b36c2fa1d22af93d5
Tobias Gesellchen <tobias.gesellchen@hypoport.de>
2014-07-29 16:17:07 +0200

    add inspect container task
    closes #7

---
+++ src/main/groovy/de/gesellix/gradle/docker/tasks/DockerInspectContainerTask.groovy
@@ -0,0 +1,23 @@
+package de.gesellix.gradle.docker.tasks
+
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class DockerInspectContainerTask extends AbstractDockerTask {
+
+  private static Logger logger = LoggerFactory.getLogger(DockerInspectContainerTask)
+
+  @Input
+  def containerId
+
+  def containerInfo
+
+  @TaskAction
+  def inspect() {
+    logger.info "running inspect container..."
+
+    containerInfo = getDockerClient().inspectContainer(getContainerId())
+  }
+}
---
+++ src/test/groovy/de/gesellix/gradle/docker/tasks/DockerInspectContainerTaskSpec.groovy
@@ -0,0 +1,31 @@
+package de.gesellix.gradle.docker.tasks
+
+import de.gesellix.docker.client.DockerClient
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class DockerInspectContainerTaskSpec extends Specification {
+
+  def project
+  def task
+  def dockerClient = Mock(DockerClient)
+
+  def setup() {
+    project = ProjectBuilder.builder().build()
+    task = project.task('dockerInspect', type: DockerInspectContainerTask)
+    task.dockerClient = dockerClient
+  }
+
+  def "delegates to dockerClient and returns result"() {
+    given:
+    task.containerId = "4711"
+
+    when:
+    task.inspect()
+
+    then:
+    1 * dockerClient.inspectContainer("4711") >> ["container": "details"]
+    and:
+    task.containerInfo == ["container": "details"]
+  }
+}
