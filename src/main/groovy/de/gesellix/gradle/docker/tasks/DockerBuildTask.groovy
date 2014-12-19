1d3a7b9d3ba540b7faccaad2bbbc8e34f80625b9
Tobias Gesellchen <tobias@gesellix.de>
2014-12-19 10:10:57 +0100

    choose a unique name for the dynamically added tarOfBuildcontext task

--- src/main/groovy/de/gesellix/gradle/docker/tasks/DockerBuildTask.groovy
+++ src/main/groovy/de/gesellix/gradle/docker/tasks/DockerBuildTask.groovy
@@ -48,7 +48,7 @@ class DockerBuildTask extends AbstractDockerTask {
   Task configure(Closure closure) {
     def configureResult = super.configure(closure)
     if (getBuildContextDirectory()) {
-      tarOfBuildcontextTask = project.task(["type": Tar], "tarOfBuildcontext") {
+      tarOfBuildcontextTask = project.task(["type": Tar], "tar of buildcontext for ${project.name}:${configureResult.name}") {
         description = "creates a tar of the buildcontext"
         from getBuildContextDirectory()
         compression = GZIP
