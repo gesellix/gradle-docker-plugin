068cf4f4aaa3a7222ac0b84b12d4da609b29e933
Tobias Gesellchen <tobias@gesellix.de>
2014-12-23 08:46:37 +0100

    use camel cased task name to make Windows systems happy

--- src/main/groovy/de/gesellix/gradle/docker/tasks/DockerBuildTask.groovy
+++ src/main/groovy/de/gesellix/gradle/docker/tasks/DockerBuildTask.groovy
@@ -48,7 +48,7 @@ class DockerBuildTask extends AbstractDockerTask {
   Task configure(Closure closure) {
     def configureResult = super.configure(closure)
     if (getBuildContextDirectory()) {
-      tarOfBuildcontextTask = project.task(["type": Tar], "tar of buildcontext for ${project.name}:${configureResult.name}") {
+      tarOfBuildcontextTask = project.task(["type": Tar], "tarBuildcontextFor${project.name.capitalize()}${configureResult.name.capitalize()}") {
         description = "creates a tar of the buildcontext"
         from getBuildContextDirectory()
         compression = GZIP
