08f92b606efa40010355b19ca1a3f519a6f5aa30
Tobias Gesellchen <tobias@gesellix.de>
2015-08-29 13:57:04 +0200

    optimize imports

--- src/main/groovy/de/gesellix/gradle/docker/tasks/DockerCopyFromContainerTask.groovy
+++ src/main/groovy/de/gesellix/gradle/docker/tasks/DockerCopyFromContainerTask.groovy
@@ -1,8 +1,6 @@
 package de.gesellix.gradle.docker.tasks
 
-import org.apache.commons.io.IOUtils
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
