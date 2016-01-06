a4a73578812ee17060235569fa1f905f6cb0bae5
Schalk W. Cronjé <ysb33r@gmail.com>
2016-01-06 13:59:46 +0000

    ISSUE #41: This adds the certPath configuration and also allows dockerHost to assume a default value from system property or environmental variable

--- src/main/groovy/de/gesellix/gradle/docker/DockerPluginExtension.groovy
+++ src/main/groovy/de/gesellix/gradle/docker/DockerPluginExtension.groovy
@@ -1,9 +1,44 @@
 package de.gesellix.gradle.docker
 
-public class DockerPluginExtension {
+import org.gradle.api.Project
 
-    def dockerHost
+/** Provides project extension class for configuring default values for all Docker tasks.
+*/
+class DockerPluginExtension {
+
+    /** Docker host url. Will default to the system property {@code docker.host.url} if that is provided,
+     * otherwise it will try to use the environment variable {@code DOCKER_HOST}. Failign that it will be
+     * null
+     *
+     */
+    String dockerHost = System.getProperty("docker.host.url") ?: System.env["DOCKER_HOST"]
+
     def proxy
     def authConfigPlain
     def authConfigEncoded
+
+    DockerPluginExtension(Project project) {
+      this.project = project
+    }
+
+    /** Allows for the setting of the path where certificates can be found.
+     *
+     * @param path ANy path object that can be resolved via {@code project.file()}
+     */
+    void setCertPath(Object path) {
+        this.certPath = path
+    }
+
+    /** Returns the certificate path as an abolute path to the certificate.
+     * If {@code certPath} was not configured it will attempt to check for system property
+     * {@code docker.cert.path} first and then for the {@code DOCKER_CERT_PATH} environment variable.
+     *
+     * @return Absolute path as a string or null.
+    */
+    String getCertPath() {
+      certPath ? project.file(certPath).absolutePath : null
+    }
+
+    private Object certPath = System.getProperty("docker.cert.path") ?: System.env["DOCKER_CERT_PATH"]
+    private Project project
 }
