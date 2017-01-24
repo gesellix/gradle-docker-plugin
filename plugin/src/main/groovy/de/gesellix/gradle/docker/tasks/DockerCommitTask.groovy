18b216564b36034641afbfed967f4c866e376bea
Tue Dissing <Tue.Dissing@enghouse.com>
2017-01-24 09:15:33 +0100

    adding docker commit task.

---
+++ plugin/src/main/groovy/de/gesellix/gradle/docker/tasks/DockerCommitTask.groovy
@@ -0,0 +1,44 @@
+package de.gesellix.gradle.docker.tasks
+
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.TaskAction
+
+class DockerCommitTask extends DockerTask {
+
+    @Input
+    def containerId
+    @Input
+    def tag
+    @Input
+    def repo
+    @Optional
+    @Input
+    def author
+    @Optional
+    @Input
+    def comment
+    @Input
+    @Optional
+    def pauseContainer
+    @Optional
+    @Input
+    def changes
+
+    DockerCommitTask() {
+        description = "Commit changes to a container"
+        group = "Docker"
+    }
+
+    @TaskAction
+    def commit() {
+        logger.info "docker commit"
+        return getDockerClient().commit(getContainerId(),[
+                                          repo   : getRepo(),
+                                          tag    : getTag(),
+                                          comment: getComment(),
+                                          author : getAuthor(),
+                                          changes: getChanges()
+                                  ])
+    }
+}
