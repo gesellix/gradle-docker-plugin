2c8de71d4be01eb740d91f047cffe3b63d420a3c
Tobias Gesellchen <tobias@gesellix.de>
2025-11-01 21:47:25 +0100

    Bump echo-server to 2025-07-27T22-12-00

--- plugin/src/test/java/de/gesellix/gradle/docker/testutil/TestImage.java
+++ plugin/src/test/java/de/gesellix/gradle/docker/testutil/TestImage.java
@@ -16,7 +16,7 @@ public class TestImage {
 
     this.isWindows = Objects.requireNonNull(dockerClient.version().getContent().getOs()).equalsIgnoreCase("windows");
     this.repository = "gesellix/echo-server";
-    this.tag = "2024-07-28T18-30-00";
+    this.tag = "2025-07-27T22-12-00";
 
     // TODO consider NOT calling prepare inside the constructor
     prepare();
