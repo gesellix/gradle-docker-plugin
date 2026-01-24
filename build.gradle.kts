ab2edf02636d347c1e0554c817e44c4fa1c814a4
Tobias Gesellchen <tobias@gesellix.de>
2026-01-24 11:52:17 +0100

    Bump Gradle Wrapper to 9.3.0
    See https://docs.gradle.org/9.3.0/release-notes.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ nexusPublishing {
 //project.apply("debug.gradle.kts")
 
 // Updating the Gradle Wrapper:
-// ./gradlew wrapper --gradle-version=9.2.0 --distribution-type=bin --gradle-distribution-sha256-sum=df67a32e86e3276d011735facb1535f64d0d88df84fa87521e90becc2d735444
+// ./gradlew wrapper --gradle-version=9.3.0 --distribution-type=bin --gradle-distribution-sha256-sum=0d585f69da091fc5b2beced877feab55a3064d43b8a1d46aeb07996b0915e0e0
 // https://gradle.org/whats-new/gradle-9/
 // Releases: https://gradle.org/releases/
 // SHA265 checksum reference: https://gradle.org/release-checksums/
@@ -68,7 +68,7 @@ nexusPublishing {
 // https://docs.gradle.org/current/userguide/gradle_daemon.html#sec:daemon_jvm_criteria
 // ./gradlew updateDaemonJvm --jvm-version=17 --jvm-vendor=corretto
 tasks.wrapper {
-  gradleVersion = "9.2.0"
+  gradleVersion = "9.3.0"
   distributionType = Wrapper.DistributionType.BIN
-  distributionSha256Sum = "df67a32e86e3276d011735facb1535f64d0d88df84fa87521e90becc2d735444"
+  distributionSha256Sum = "0d585f69da091fc5b2beced877feab55a3064d43b8a1d46aeb07996b0915e0e0"
 }
