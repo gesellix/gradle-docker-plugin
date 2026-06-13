258acd9f9e393a4715f87583f472693c75193dd2
Tobias Gesellchen <tobias@gesellix.de>
2026-06-13 19:15:08 +0200

    updateDaemonJvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ nexusPublishing {
 //project.apply("debug.gradle.kts")
 
 // Updating the Gradle Wrapper:
-// ./gradlew wrapper --gradle-version=9.4.0 --distribution-type=bin --gradle-distribution-sha256-sum=60ea723356d81263e8002fec0fcf9e2b0eee0c0850c7a3d7ab0a63f2ccc601f3
+// ./gradlew wrapper --gradle-version=9.5.1 --distribution-type=bin --gradle-distribution-sha256-sum=bafc141b619ad6350fd975fc903156dd5c151998cc8b058e8c1044ab5f7b031f
 // https://gradle.org/whats-new/gradle-9/
 // Releases: https://gradle.org/releases/
 // SHA265 checksum reference: https://gradle.org/release-checksums/
@@ -68,7 +68,7 @@ nexusPublishing {
 // https://docs.gradle.org/current/userguide/gradle_daemon.html#sec:daemon_jvm_criteria
 // ./gradlew updateDaemonJvm --jvm-version=17 --jvm-vendor=corretto
 tasks.wrapper {
-  gradleVersion = "9.4.0"
+  gradleVersion = "9.5.1"
   distributionType = Wrapper.DistributionType.BIN
-  distributionSha256Sum = "60ea723356d81263e8002fec0fcf9e2b0eee0c0850c7a3d7ab0a63f2ccc601f3"
+  distributionSha256Sum = "bafc141b619ad6350fd975fc903156dd5c151998cc8b058e8c1044ab5f7b031f"
 }
