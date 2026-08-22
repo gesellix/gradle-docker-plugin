204a80d25d11c0a4a9b15eabdd59d09b7f38333a
Tobias Gesellchen <tobias@gesellix.de>
2026-08-22 23:41:26 +0200

    Use io.github.ben-manes.versions plugin id

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   id("java-gradle-plugin")
   id("maven-publish")
   id("signing")
-  id("com.github.ben-manes.versions")
+  id("io.github.ben-manes.versions")
   id("org.sonatype.gradle.plugins.scan")
   id("com.gradle.plugin-publish")
   id("io.freefair.maven-central.validate-poms")
